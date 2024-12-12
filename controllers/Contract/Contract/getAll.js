969a3cd81ec08765c023785f29c023291d7d6090
nikitabaladha <nikitabaladha1996@gmail.com>
2024-12-12 06:50:42 +0530

    contract get all api change fro sending data according to needed value

--- controllers/Contract/Contract/getAll.js
+++ controllers/Contract/Contract/getAll.js
@@ -14,6 +14,17 @@ async function getAll(req, res) {
         select: "contractName",
       });
 
+    const now = new Date();
+    const currentMonth = now.getMonth(); // 0-11
+    const currentYear = now.getFullYear();
+
+    const startOfWeek = new Date(now);
+    startOfWeek.setDate(now.getDate() - now.getDay() + 1); // Adjust to Monday
+
+    // Calculate the end of the current week (Sunday)
+    const endOfWeek = new Date(startOfWeek);
+    endOfWeek.setDate(startOfWeek.getDate() + 6);
+
     const response = contracts.map((contract) => ({
       contractId: contract.id,
       employeeName: contract.employeeId ? contract.employeeId.name : "N/A",
@@ -35,9 +46,48 @@ async function getAll(req, res) {
       description: contract.description,
     }));
 
+    const totalValue = response.reduce(
+      (acc, contract) => acc + contract.value,
+      0
+    );
+
+    const totalValueOfThisMonth = contracts.reduce((acc, contract) => {
+      const startDate = new Date(contract.startDate);
+      if (
+        startDate.getMonth() === currentMonth &&
+        startDate.getFullYear() === currentYear
+      ) {
+        return acc + contract.value;
+      }
+      return acc;
+    }, 0);
+
+    const totalValueThisWeek = contracts.reduce((acc, contract) => {
+      const startDate = new Date(contract.startDate);
+      if (startDate >= startOfWeek && startDate <= endOfWeek) {
+        return acc + contract.value;
+      }
+      return acc;
+    }, 0);
+
+    const last30DaysValue = contracts.reduce((acc, contract) => {
+      const startDate = new Date(contract.startDate);
+      const thirtyDaysAgo = new Date();
+      thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
+
+      if (startDate >= thirtyDaysAgo) {
+        return acc + contract.value;
+      }
+      return acc;
+    }, 0);
+
     return res.status(200).json({
       message: "Contracts fetched successfully!",
       data: response,
+      totalValue: totalValue,
+      totalValueOfThisMonth: totalValueOfThisMonth,
+      totalValueOfThisWeek: totalValueThisWeek,
+      last30DaysValue: last30DaysValue,
     });
   } catch (error) {
     console.error("Error fetching contracts:", error.message);
