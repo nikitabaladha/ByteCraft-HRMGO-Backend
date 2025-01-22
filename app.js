e852a3903fa98836206871b8ce6bb24f969907a4
akashbalodi <akashbalodi17@gmail.com>
2025-01-22 17:14:21 +0530

    Messenger

--- app.js
+++ app.js
@@ -12,6 +12,48 @@ const cors = require("cors");
 
 const app = express();
 
+const Users = require("./models/User")
+
+const io = require("socket.io")(3030, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+//Socket.io
+let users = [];
+io.on("connection", (socket) => {
+  socket.on("addUser", (userId) => {
+    const isUserExist = users.find((user) => user.userId === userId);
+    if (!isUserExist) {
+      const user = { userId, socketId: socket.id };
+      users.push(user);
+      io.emit("getUsers", users);
+    }
+  });
+
+  socket.on('sendMessage', async({ senderId, receiverId, message, conversationId}) => {
+    const receiver = users.find(user => user.userId === receiverId);
+    const sender = users.find(user => user.userId === senderId)
+    const user = await Users.findById(senderId)
+    if(receiver) {
+      io.to(receiver.socketId).to(sender.socketId).emit('getMessage', {
+        senderId,
+        receiverId,
+        conversationId,
+        message,
+        user: { id: user._id, name: user.name, email: user.email}
+      })
+    }
+  })
+
+  socket.on('disconnet', () => {
+    users = users.filter(user => user.socketId !== socket.id)
+    io.emit('getUsers', users)
+  })
+  // io.emit('getUsers', socket.userId)
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 connectDB();
--- controllers/Messenger/Message/getMessage.js
+++ controllers/Messenger/Message/getMessage.js
@@ -1,19 +1,43 @@
+const Conversation = require("../../../models/Conversation");
 const Messages = require("../../../models/Messenger");
-const Users = require("../../../models/User")
+const Users = require("../../../models/User");
 
-const getMessageById = async(req, res) => {
-    try{
-        const conversationId= req.params.conversationId;
-        if(conversationId == "new") return res.status(200).json([])
-        const messages = await Messages.find({conversationId});
-        const messageUserData = Promise.all(messages.map(async (message) => {
-            const user = await Users.findById(message.senderId);
-            return { user: {id: user._id, email: user.email, name: user.name, profileImage: user.profileImage}, message: message.message}
-        }))
-        res.status(200).json(await messageUserData);
-    } catch (error) {
-        console.log('Error', error)
+const getMessageById = async (req, res) => {
+  try {
+    const checkMessages = async (conversationId) => {
+      const messages = await Messages.find({ conversationId });
+      const messageUserData = Promise.all(
+        messages.map(async (message) => {
+          const user = await Users.findById(message.senderId);
+          return {
+            user: {
+              id: user._id,
+              email: user.email,
+              name: user.name,
+              profileImage: user.profileImage,
+            },
+            message: message.message,
+          };
+        })
+      );
+      res.status(200).json(await messageUserData);
+    };
+    const conversationId = req.params.conversationId;
+    if (conversationId == "new") {
+      const checkConversation = await Conversation.find({
+        members: { $all: [req.query.senderId, req.query.receiverId] },
+      });
+      if (checkConversation.length > 0) {
+        checkMessages(checkConversation[0]._id);
+      } else {
+        return res.status(200).json([]);
+      }
+    } else {
+      checkMessages(conversationId);
     }
-}
+  } catch (error) {
+    console.log("Error", error);
+  }
+};
 
 module.exports = getMessageById;
